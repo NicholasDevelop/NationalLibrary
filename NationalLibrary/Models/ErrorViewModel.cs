3b611ca4526ae8d0db4cada18d535d43d53701a5
NicholasDevelop <marcaccini.n@gmail.com>
2022-10-14 17:43:52 +0200

    Aggiungere i file di progetto.

---
+++ NationalLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NationalLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
