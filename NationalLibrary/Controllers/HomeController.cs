8db4b65e7afa2496fb9d1d4e3af63c63855dcfed
Emanuele De Rosa <85497974+OliverQueenArrowTheBest@users.noreply.github.com>
2022-10-18 18:54:34 +0200

    Added Frontend addEmployee page

--- NationalLibrary/Controllers/HomeController.cs
+++ NationalLibrary/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using NationalLibrary.Data;
 using NationalLibrary.Metodi;
 using NationalLibrary.FinalViews;
+using System.Diagnostics.Eventing.Reader;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace NationalLibrary.Controllers
 {
@@ -16,43 +18,66 @@ namespace NationalLibrary.Controllers
 			_logger = logger;
 			this.ctx = ctx;
 		}
-
+		private static UserFinalView userFinal;
 		public IActionResult Index()
 		{
-			//DataQueries.EditUser("ABSDEL03A03M849U", "Librarian", "Mattia", "Romagnoli", "3339998888", new DateTime(1996, 10, 19), String.Empty,
-			//"Siena", "Via Dante Alighieri 8", 20040, "SI",
+			//DataQueries.InsertUser("ABSDEL03A03M849U", "Librarian", "Mattia", "Romagnoli", "3339998888", new DateTime(1975, 10, 19),
+			//"Genova", "Via Dante Alighieri 8", 20040, "SI",
 			//"AA01890BB", "C.I.", "Siena", new DateTime(2030, 03, 25),
-			//"mattiaromagnoli1@gmail.com", "RomansLibrarian", "password", ctx);
+			//"mattiaromagnoli1@gmail.com", "RomansLibrarian", "password", String.Empty, ctx);
 			//DataQueries.InsertUser
 			//(
-			//"ABSDEL03A03M849U", "Librarian", "Mattia", "Romagnoli", "3339998888", new DateTime(1996,10,19),
-			//"Siena", "Via Dante Alighieri 8", 20040, "SI",
-			//"AA01890BB", "C.I.", "Siena", new DateTime(2030,03,25),
-			//"mattiaromagnoli1@gmail.com", "Romans", "password", String.Empty, ctx
+			//"ADMINR03A03M849U", "Admin", "Mattia", "Romagnoli", "3409998888", new DateTime(1996,12,05),
+			//"Siena", "Via Dante Alighieri 8", 69040, "SI",
+			//"AD01890MI", "C.I.", "Siena", new DateTime(2030,12,25),
+			//"mattiaromagnoli1@gmail.com", "Admin", "Admin", String.Empty, ctx
+			//);
+			//DataQueries.InsertUser
+			//(
+			//"USERMR03A03M849U", "User", "Brian", "Romagnoli", "3400768888", new DateTime(2003,01,02),
+			//"Siena", "Via Dante Alighieri 8", 69040, "SI",
+			//"AZ01890MI", "C.I.", "Siena", new DateTime(2030,12,25),
+			//"mattiaromagnoli1@gmail.com", "Brian", "password", String.Empty, ctx
 			//);
 			//ViewsLoaders.UserFinalViewList(ctx);
 			return View();
 		}
-		public IActionResult addEmployee()
+		public IActionResult router()
 		{
-			return View();
+			return RedirectToAction("dashboard", userFinal);
+		}
+		public IActionResult addEmployee(UserFinalView user)
+		{
+			return View(user);
 		}
 
-		public IActionResult loginPage(Book book)
+		public IActionResult loginPage()
 		{
-			return View(book);
+			return View();
 		}
 		public IActionResult userDashboard(User user)
 		{
 			return View(user);
 		}
 
-        public IActionResult viewBook(Book book)
-        {
-            return View(book);
-        }
+		public IActionResult viewBook(Book book)
+		{
+			return View(book);
+		}
 
-        public IActionResult dashboard(UserFinalView user)
+		public IActionResult insertEmployee(UserFinalView user)
+		{
+			Console.WriteLine(user.Name);
+			Console.WriteLine(user.Username);
+			Console.WriteLine(user.MobilePhone);
+			DataQueries.InsertUser(user.FiscalCode, "Librarian", user.Name, user.Surname, user.MobilePhone, user.BirthDate, user.City, user.Street,
+	user.CAP, user.Province, user.DocumentNumber, user.DocumentType,
+	user.ReleasedBy, user.ExpiredOn, user.Email, user.Username, user.Password, String.Empty, ctx);
+			user = userFinal;
+			return RedirectToAction("dashboard", userFinal);
+		}
+
+		public IActionResult dashboard(UserFinalView user)
 		{
 			UserFinalView type = ViewsLoaders.getUserType(user.Username, user.Password, ctx);
 			if (string.IsNullOrEmpty(type.Type))
@@ -63,6 +88,7 @@ namespace NationalLibrary.Controllers
 			switch (type.Type)
 			{
 				case "Admin":
+					userFinal = type;
 					return View(type);
 				case "User":
 					return RedirectToAction("userDashboard", type);
